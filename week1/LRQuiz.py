a44126da792894c28fcbafd2ac27073aabddd316
Naren Mudivarthy <nmudivar@cisco.com>
2017-02-05 20:06:39 -0800

    Added AND, NOT and OR Perceptrons for lesson Intro to Neural Networks

--- week1/LRQuiz.py
+++ week1/LRQuiz.py
@@ -1,12 +1,11 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd
+
 # import matplotlib.pyplot as plt
 
-
-
 bmi_life_model = LinearRegression()
-
 bmi_life_data = pd.read_csv('bmi_and_life_expectancy.csv')
+
 print bmi_life_data.head()
 
 x = bmi_life_data[['BMI']]
@@ -17,7 +16,6 @@ bmi_life_model.fit(x, y)
 # plt.scatter(x, y)
 # plt.plot(x, bmi_life_model.predict(x))
 # plt.show()
-
 
 laos_life_exp = bmi_life_model.predict(21.07931)
 print laos_life_exp
